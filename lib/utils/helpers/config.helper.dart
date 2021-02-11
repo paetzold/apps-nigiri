eb8d40eefd94c82ad9614af284639c8cb5639db2
Kay P <kay.paetzold@gmail.com>
2021-02-11 09:32:38 +0100

    appConfig was on ignore list

--- lib/utils/helpers/config.helper.dart
+++ lib/utils/helpers/config.helper.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 
-const String _CONFIG_FILE_PATH = 'assets/config.json';
+const String _CONFIG_FILE_PATH = 'assets/appConfig.json';
 
 Future<Map<String, dynamic>> loadConfigFile() async {
   String jsonString = await rootBundle.loadString(_CONFIG_FILE_PATH);
